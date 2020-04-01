36b8aa6bf0c59bd9ae09686c943a80fcdaa485f8
Smith, Charlie <smh@pakuranga.school.nz>
2020-04-01 20:23:02 +1300

    Basics in place. Working connection to database and a single working template.

--- AthleticsDay/app.py
+++ AthleticsDay/app.py
@@ -1,19 +1,30 @@
-"""
-This script runs the application using a development server.
-It contains the definition of routes and views for the application.
-"""
+from flask import Flask, render_template, redirect, request
+import pymysql
 
-from flask import Flask
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 
+def create_connection():
+    return pymysql.connect(  
+        host = '127.0.0.1',
+        user = 'root',
+        password = '13com',
+        db = 'AthleticsDay',
+        charset = 'utf8mb4',
+        cursorclass = pymysql.cursors.DictCursor
+    )
 
 @app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World!"
+def index():
+    connection = create_connection()
+    with connection.cursor() as cursor:
+        sql = "SELECT eventID, eventName FROM tblEvents;"
+        cursor.execute(sql)
+        events = cursor.fetchall()
+        connection.close()
+    return render_template("index.html", events = events)
 
 if __name__ == '__main__':
     import os
