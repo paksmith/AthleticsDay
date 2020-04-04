92735ea2bf023a3f8f01a7a8311e61534ca38cd5
Smith, Charlie <smh@pakuranga.school.nz>
2020-04-04 17:58:18 +1300

    Registration and login functioning

--- AthleticsDay/app.py
+++ AthleticsDay/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session
+from hashlib import md5
 import pymysql
 
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
+
+ROLE_USER = 1
+ROLE_ADMIN = 2
+ROLE_TEACHER = 3
 
 def create_connection():
     return pymysql.connect(  
@@ -29,12 +34,56 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        pass
+        userName = request.form["userName"]
+        password = request.form["password"]
+        password = md5(password.encode()).hexdigest()
+        connection = create_connection()
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM tblusers WHERE userName = %s AND password = %s"
+            vals = (userName, password)
+            cursor.execute(sql, vals)
+            users = cursor.fetchall()
+        connection.close()
+        if len(users) == 0:
+            #Login failed
+            return render_template("login.html")
+        else:
+            #Login succeeded'
+            user = users[0]
+            session['userName'] = user['roleID']
+            session['firstName'] = user['firstName']
+            session['lastName'] = user['lastName']
+            session["loggedIn"] = True
+            session["roleId"] = user['roleID']
+            return redirect('/')
     else:
         return render_template("login.html")
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        #Get values from form, insert into tblUsers.
+        #TODO add validation (such as check that username is not already taken)
+        userName = request.form["userName"]
+        firstName = request.form["firstName"]
+        lastName = request.form["lastName"]
+        password = request.form["password"]
+        password = md5(password.encode()).hexdigest()
+        connection = create_connection()
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO tblusers(userName, firstName, lastName, password, roleID) VALUES(%s, %s, %s, %s, %s)"
+            vals = (userName, firstName, lastName, password, ROLE_USER)
+            cursor.execute(sql, vals)
+            connection.commit()
+            connection.close()
+        return redirect("/login")
+    else:
+        return render_template("register.html")
+
 if __name__ == '__main__':
     import os
+    app.secret_key = os.urandom(12)
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
